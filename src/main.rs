d9059ee3f3e150fdc12b2707171b73c174152c21
Yuki Kodama <endflow.net@gmail.com>
2019-10-31 23:30:15 +0900

    Decode message

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use serde::{Deserialize, Serialize};
 use std::convert::TryInto;
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, Read, Write};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Message {
+    payload: String,
+}
+
+#[derive(Serialize, Debug)]
+struct ErrorResponse {
+    error: String,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     loop {
@@ -10,21 +21,23 @@ fn main() -> Result<(), Box<dyn Error>> {
         let bytes: Vec<u8> = handle.bytes().map(|i| i.unwrap()).collect();
         let size = u32::from_le_bytes(bytes.as_slice().try_into().unwrap());
 
-        let mut file = File::create("/Users/kuy/Work/au2far-ocr/size.txt")?;
-        write!(file, "SIZE: {}", size)?;
-        file.flush()?;
-
         // Receive message body
         let mut buf = String::new();
         let mut handle = io::stdin().take(size as u64);
-        handle.read_to_string(&mut buf)?;
+        let len = handle.read_to_string(&mut buf)?;
+
+        let message: Message = serde_json::from_str(&buf)?;
 
         let mut file = File::create("/Users/kuy/Work/au2far-ocr/output.txt")?;
-        write!(file, "{}", buf)?;
+        write!(
+            file,
+            "size={}\nread={}\ndata={}",
+            size, len, message.payload
+        )?;
         file.flush()?;
 
         // Send response
-        let res = "{\"res\":\"OK\"}";
+        let res = "{\"payload\":\"123456\"}";
         io::stdout().write_all(&u32::to_le_bytes(res.len() as u32))?;
         write!(io::stdout(), "{}", res)?;
         io::stdout().flush()?;
